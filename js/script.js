57938ae99e378a6549abef38a2ba0a4405cde425
Jefferson Souza <jeffersonsouza03@hotmail.com>
2024-10-02 17:51:33 -0300

    improve: animar secao numbers

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+import AnimaNumeros from "./modules/anima-numeros.js";
+
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
+
+document.querySelectorAll(".number").forEach((element) => {
+  new AnimaNumeros(element, 3000);
+});
